fd23b9c17c36e72f096c6e156f7cb410c35b7441
Lalithrl <lalithrl5432@gmail.com>
2023-08-01 17:48:24 +0530

    Add project files.

---
+++ WebGAME/Gameform.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="WebGAME." Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Gameform.aspx" Inherits="WebGAME.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Welcome to the number Guessing Game!</h1>
+    <asp:Label ID="lblFeedback" runat="server" Text="Feedback"></asp:Label>
+    <br />
+    <asp:TextBox ID="txtGuess" runat="server" OnTextChanged="txtGuess_TextChanged"></asp:TextBox>
+    <asp:Button ID="btnSubmit" Text="Submit Guess" OnClick="SubmitButton_Click" runat="server"  />
+</asp:Content>
---
+++ WebGAME/WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebGAME
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string connectionString = "Data Source=RAGUNATH947\MSSQLSERVER01;Initial Catalog = EmpyDB; Integrated Security = True";
+
+        private int generatedNumber;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                generatedNumber = GeneratedRandomNumber();
+                Session["GeneratedNumber"] = generatedNumber;
+            }
+                
+        }
+
+        private int GeneratedRandomNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void SubmitButton_Click (object sender, EventArgs e)
+        {
+            int playerGuess;
+            if (int.TryParse(txtGuess.Text, out playerGuess))
+            {
+                int generatedNumber = (int)Session["GeneratedNumber"];
+                string playerName = "Player";
+
+                string query = "INSERT INTO Guesses (PlayerName, GuessValue, GeneratedNumber, DateTime) VALUES (@PlayerName, @GuessValue, @GeneratedNumber, @DateTime)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@PlayerName", playerName);
+                        command.Parameters.AddWithValue("@GuessValue", playerGuess);
+                        command.Parameters.AddWithValue("@GeneratedNumber", generatedNumber);
+                        command.Parameters.AddWithValue("@DateTime", DateTime.Now);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                if (playerGuess == generatedNumber)
+                {
+                    lblFeedback.Text = "Congratulations! You guessed it right!";
+                    txtGuess.Enabled = false;
+                    btnSubmit.Enabled = false;
+                }
+                else if (playerGuess < generatedNumber)
+                {
+                    lblFeedback.Text = "Too low! Try again.";
+                }
+                else
+                {
+                    lblFeedback.Text = "Too high! Try again.";
+                }
+            }
+            else
+            {
+                lblFeedback.Text = "Please enter a valid number.";
+            }
+        }
+
+        private int GenerateRandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(1, 101); 
+
+
+
+
+
+
+        }
+    }
+}
